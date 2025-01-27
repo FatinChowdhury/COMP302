5f330b74b936065ef6e359d7f3060490e04550fc
FatinChowdhury <122334684+FatinChowdhury@users.noreply.github.com>
2025-01-27 11:01:45 -0500

    Create types.mli

---
+++ notes/L1/types.mli
@@ -0,0 +1,14 @@
+"hello " ^ "world" ;; 
+(would return)
+- : string = "hello world"
+let name = "fatin" ;;
+val name : string = "fatin"    (the : means "has type")
+
+
+let name = "nitaf" ;;
+val name : string = "nitaf"
+
+
+name ;; 
+- : string = "fatin"
+(it uses most recent value of the "variable")
