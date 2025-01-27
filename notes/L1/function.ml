568ba305a20aa5ae7a2621f21296e7da2a29d436
FatinChowdhury <122334684+FatinChowdhury@users.noreply.github.com>
2025-01-27 11:05:46 -0500

    Create function.ml

---
+++ notes/L1/function.ml
@@ -0,0 +1,10 @@
+let greet name = "hello " ^ name ;;
+val greet : string -> string = <fun>
+            (input)   (output)
+
+// to run the function
+greet "fatin" ;;
+- : string = "hello fatin"
+
+greet name ;;
+- : string = "hello nitaf"
